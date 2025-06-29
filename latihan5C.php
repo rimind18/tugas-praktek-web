7796e283951abbca50f30e0c050909fd85859f44
rimind18 <fahrimuhaiminn@gmail.com>
2025-06-29 21:36:40 +0700

    Update latihan5C.php

--- latihan5C.php
+++ latihan5C.php
@@ -1,18 +1,43 @@
 <?php
-$nama = $_GET['nama'];
-$email = $_GET['email'];
-$jurusan = $_GET['jurusan'];
-?>
+$mahasiswa = [
+    "nama" => "fahri muahaimin",
+    "email" => "fahrimuhaiminn@gmail.com",
+    "jurusan" => "Sistem Informasi, Universitas Islam Negeri Sulthan Thaha Saifuddin Jambi "
+];
 
+// Tampilkan data
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Detail Mahasiswa</title>
+    <title>Profil Mahasiswa</title>
+    
+    <style>
+        .profile-box {
+            width: 400px;
+            margin: 50px auto;
+            border: 2px solid #ccc;
+            border-radius: 8px;
+            padding: 30px;
+            text-align: center;
+        }
+        .profile-img {
+            width: 120px;
+            height: 120px;
+            border-radius: 50%;
+            background: #eee;
+            margin: 0 auto 20px auto;
+        }
+    </style>
 </head>
 <body>
-    <h2>Detail Mahasiswa</h2>
-    <p>Nama: <?= $nama ?></p>
-    <p>Email: <?= $email ?></p>
-    <p>Jurusan: <?= $jurusan ?></p>
+    <div class="profile-box">
+        <h2>Profil Mahasiswa</h2>
+        <div class="profile-img"></div>
+        <h3><?php echo $mahasiswa['nama']; ?></h3>
+        <p><?php echo $mahasiswa['email']; ?></p>
+        <p><?php echo $mahasiswa['jurusan']; ?></p>
+        <a href="latihan5b.php">Kembali</a>
+    </div>
 </body>
 </html>
